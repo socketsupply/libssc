8cbf4df773ef6bd8abeff10d7543ab4b582d00e4
jwerle <joseph.werle@gmail.com>
2022-04-19 18:52:16 -0400

    chore(): disable brief.mk with 'NO_BRIEF

--- mk/brief.mk
+++ mk/brief.mk
@@ -2,5 +2,7 @@ BRIEF_FORMAT ?= "  %s   \t%s\n"
 BRIEF_ECHO ?= printf $(BRIEF_FORMAT) "$(1)" "$(2)" | tr '\n' ' ' && echo
 BRIEFC += AR CC CP LN MKDIR RM STRIP
 
+ifndef NO_BRIEF
 -include ./deps/brief.mk/brief.mk
 -include ../deps/brief.mk/brief.mk
+endif
